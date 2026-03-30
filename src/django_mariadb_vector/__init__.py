6336370a19f579bb2784d0810f53c99817ec16bb
lexxai <lexxai@gmail.com>
2026-03-31 01:58:23 +0300

    [0.2.0] Added __version__

--- src/django_mariadb_vector/__init__.py
+++ src/django_mariadb_vector/__init__.py
@@ -1,4 +1,24 @@
+import importlib.metadata
+
+
 from .fields import MariaDBVectorField, MariaDBVectorIndex, VectorDistances
 from .functions import VecFromText, VecToText, VecDistanceCosine, VecDistanceEuclidean, Search, VecDistance
 
-__all__ = ["VectorDistances", "MariaDBVectorField", "MariaDBVectorIndex", "VecFromText", "VecToText", "VecDistanceCosine", "VecDistanceEuclidean", "Search", "VecDistance"]
+
+try:
+    __version__ = importlib.metadata.version("django-mariadb-vector")
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.1"
+
+__all__ = [
+    "VectorDistances",
+    "MariaDBVectorField",
+    "MariaDBVectorIndex",
+    "VecFromText",
+    "VecToText",
+    "VecDistanceCosine",
+    "VecDistanceEuclidean",
+    "Search",
+    "VecDistance",
+    "__version__",
+]
