ed66fd30df820b77f2faabd112add522a2a3f073
lexxai <lexxai@gmail.com>
2026-03-31 03:41:50 +0300

    [0.2.0] Added FillDataTest for Docker test version

--- tests/models.py
+++ tests/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 from django_mariadb_vector.fields import MariaDBVectorField, MariaDBVectorIndex
 
+
 class VectorModel(models.Model):
     embedding = MariaDBVectorField(dimensions=3)
 
     class Meta:
-        indexes = [
-            MariaDBVectorIndex(fields=['embedding'], name='vector_idx', dimensions=3)
-        ]
+        indexes = [MariaDBVectorIndex(fields=["embedding"], dimensions=3)]
+
+
+class VectorModelBinary(models.Model):
+    embedding = MariaDBVectorField(dimensions=3, binary_response=True)
+
+    class Meta:
+        indexes = [MariaDBVectorIndex(fields=["embedding"], dimensions=3)]
